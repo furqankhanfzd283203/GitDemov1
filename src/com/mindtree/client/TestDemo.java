f361bc94675776f1ba1879b30b325e44d7879e45
Furqan <furqankhanfzd283203@gmail.com>
2021-08-05 10:37:45 +0530

    first commit

---
+++ src/com/mindtree/client/TestDemo.java
@@ -0,0 +1,15 @@
+package com.mindtree.client;
+
+public class TestDemo {
+	public static void main(String[] args){
+		String hungry="yes";
+		if(hungry=="yes"){
+			System.out.println("eat something");
+		}
+		else
+		{
+			System.out.println("Do work");
+		}
+	}
+
+}
